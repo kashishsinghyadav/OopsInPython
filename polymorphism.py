e9815b93167721ba7ad0f6e94b49cce3610ce6ce
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2023-11-06 21:38:07 +0530

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,7 +1,7 @@
 # The word polymorphism is derived from the two words i.e. ploy and morphs. Poly means many and morphs means forms. It allows us to create methods with the same name but different method signatures. It allows the developer to create clean, sensible, readable, and resilient code.
 
 class Polymorphism: # method overloaing / static binding 
-    def expre(self,name=None):
+    def expre(self,name=None):#method overloaing is achive using default paramter or keyword parameter
         if name is not None:
             print('my name is ' + name)
 
